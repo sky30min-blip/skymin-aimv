43a01af39994306f240e206573484dd279b03921
sky30min-blip <sky30min@gmail.com>
2026-01-27 23:58:16 +0900

    Update __init__.py

---
+++ tabs/__init__.py
@@ -0,0 +1,8 @@
+"""
+tabs/__init__.py - 탭 모듈 초기화
+"""
+from . import theme_expander
+from . import lyrics
+from . import character
+from . import storyboard
+__all__ = ['theme_expander', 'lyrics', 'character', 'storyboard']
